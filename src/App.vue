f8430450bc63b21f063fb3cf078265f7f979e950
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-05-21 13:36:24 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Stocks from './components/Stocks.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="title">Stock App</h2>
+    <Stocks />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="stock">
+    <div class="stock-item">
+      <div class="stock-item__info">
+        <div class="stock-info__cover">
+          <img src="" alt="">
+        </div>
+        <h3 class="stock-item__title">
+          Apple Inc
+          <span>AAPL</span>
+        </h3>
+      </div>
+      <span class="stock-info__price">127.35 $</span>
+    </div>
+  </div>
+</template>
